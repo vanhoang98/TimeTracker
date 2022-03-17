0abf7dbec4908d774e414531dadea1bc5fa9e4ab
Tran Van Hoang <vanhoang98>
2022-03-17 16:50:04 +0700

    add layout login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,17 +2,25 @@
 
 @section('content')
 <div class="container">
+    <div class="h-logo" style="padding-bottom: 20px;">
+        <h1>
+            <a href="/">
+                <div class="h-logo__img" style="text-align: center">
+                    <img src="{{ asset('images/cmn-pct-logo02.png') }}" style="width: 50%" alt="" loading="lazy">
+                </div>
+            </a>
+        </h1>
+    </div>
+
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+            <div class="card" style="background-color: #0d3247; border: none;">  
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right text-white-bold">メールアドレス</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -26,7 +34,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right text-white-bold">パスワード</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -44,8 +52,8 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                    <label class="form-check-label text-white-bold" for="remember">
+                                        ログイン状態を記憶
                                     </label>
                                 </div>
                             </div>
@@ -53,15 +61,15 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                <button type="submit" class="btn btn-primary text-white-bold login-button">
+                                    ログイン
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
+                                @endif --}}
                             </div>
                         </div>
                     </form>
